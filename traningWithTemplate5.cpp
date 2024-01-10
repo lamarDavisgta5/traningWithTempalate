9c2b770d13a15700c0bfd9bea199c9003b535d7f
lamar davis <155438522+lamarDavisgta5@users.noreply.github.com>
2024-01-10 12:42:44 -0800

    Update traningWithTemplate5.cpp
    the program has been finished
    aim : caculate the friction force  , acceleration break and the time between break and stop when a car is driving in a certain speed and break the car when the driver see a barrier 
    and make random numbers between acceleration break , time and friction force and power or root them and find odd and even numbers 
    write on standard C++17
    compiled with gcc

--- traningWithTemplate5.cpp
+++ traningWithTemplate5.cpp
@@ -4,6 +4,7 @@
 # include <string>
 # include <cmath>
 # include <math.h>
+constexpr auto gravity { 9.8 };
 
 void car () {
   std :: cout << "A car is moving in a speed and by seeing the barrier " << '\n';
@@ -17,12 +18,13 @@ void car () {
 double option () {
   again:
   std :: cout << "enter 1 : caculete the stop accelaration " << '\n';
-  std :: cout << "enter 2 : caculate the time of the between the break " << '\n';
+  std :: cout << "enter 2 : caculate the time of the between the break and stop " << '\n';
   std :: cout << "enter 3 : caculate the friction force between tire of the car and the surface " << '\n';
+  std :: cout << "enter 4 : caculate the item 1 , 2 , 3 " << '\n';
   double opt {};
   std :: cin >> opt;
   
-  if ( opt != 1 && opt != 2 && opt != 3 ) {
+  if ( opt != 1 && opt != 2 && opt != 3 && opt != 4 ) {
     std :: cout << "you must enter 1 , 2 , 3 " << '\n';
     goto again;
   }
@@ -241,6 +243,28 @@ void random ( T object1 , T object2 ) {
       }
 
       std :: cout << '\n'; 
+      bool tOrF { trueOrFalse () };
+
+      if ( tOrF ) {
+        std :: cout << "you entered true " << '\n';
+        std :: cout << '\n';
+        std :: cout << "odds : 1" << '\n';
+        std :: cout << "evens : 0 " << '\n';
+
+        for ( int counter { 1 }; counter <=SN; ++ counter ) {
+          std :: cout << period ( random3 ) << " = " << period ( random4 ) % 2 <<'\t';
+
+          if ( counter % 10 == 0 )
+            std :: cout << '\n'; 
+
+        }
+
+
+      }
+
+      else 
+        std :: cout << "you entere false " << '\n';
+
 
   
 
@@ -260,6 +284,27 @@ void random ( T object1 , T object2 ) {
       } 
 
       std :: cout << '\n';
+      bool tOrF { trueOrFalse () };
+
+      if ( tOrF ) {
+        std :: cout << "you entered true " << '\n';
+        std :: cout << '\n';
+        std :: cout << "odds : 1 " << '\n';
+        std :: cout << "evens : 0 " << '\n'; 
+
+        for ( int counter { 1 }; counter <= SN; ++ counter ) {
+          std :: cout << period ( random3 ) << " = " << period ( random4 ) % 2 << '\t';
+
+          if ( counter % 10 == 0 ) 
+            std :: cout << '\n'; 
+
+
+        }
+
+      }
+
+      else
+        std :: cout << "you entered false " << '\n';
 
 
     }
@@ -272,97 +317,231 @@ void random ( T object1 , T object2 ) {
 
 }
 
+auto getSpeed () {
+  std :: cout << "enter the speed of the car in m/s : " << '\n';
+  double speed {};
+  std :: cin >> speed;
+
+return speed;
+}
+
+auto displacement () {
+  std :: cout << "enter the displacement after the car stop by seeing the barrier in m : " << '\n';
+  double d {};
+  std :: cin >> d;
+
+return d;
+}
+
+auto getMass () {
+  std :: cout << "enter the mass of the car in kg : " << '\n';
+  double mass {};
+  std :: cin >> mass;
+
+return mass;
+}
+
+
+
+double accelaration () {
+  double V { getSpeed () };
+  double D { displacement () };
+  double VpowerTwo { std :: pow ( V , 2 ) };
+  std :: cout << "the stop accelaration of the car is : " << ( 0 - VpowerTwo ) /  ( 2 * D ) << " m / s2 " << '\n';
+
+return ( 0 - VpowerTwo ) / ( 2 * D );
+}
+
+double time () {
+  double V { getSpeed () };
+  double D { displacement () };
+  double VpowerTwo { std :: pow ( V , 2 ) };
+  double accelaration { ( 0 - VpowerTwo ) /  ( 2 * D ) };
+
+  if ( V / accelaration > 0 ) 
+    std :: cout << "the time betweem breake and stop is : " << static_cast < int > ( V / accelaration )  << 's' << '\n';
+
+  else if ( V / accelaration < 0 ) 
+    std :: cout << "the time betweem breake and stop is : " << static_cast < int > ( V / accelaration ) * -1  << 's' << '\n';
+
+
+return  ( ( V / accelaration ) > 0 ) ? V / accelaration : -1 * ( V / accelaration );
+}
+
+
+double frictionForce () {
+  double V { getSpeed () };
+  double D { displacement () };
+  double VpowerTwo { std :: pow ( V , 2 ) };
+  double accelaration { ( 0 - VpowerTwo ) /  ( 2 * D ) };
+  double M { getMass () };
+
+  if (  ( accelaration * M ) > 0 )
+    std :: cout << "the friction force between the tire and the surface is : " << accelaration * M<< 'N' <<'\n';
+
+  else if  (  ( accelaration * M ) < 0 )
+    std :: cout << "the friction force between the tire and the surface is : " <<  -1 * ( accelaration * M ) << 'N' <<'\n';
+
+return ( ( accelaration * M ) > 0 ) ? accelaration * M : -1 * ( accelaration * M ); 
+}
+
+
+double first_Item ();
+double second_Item ();
+bool makeRandomNumbers ();  
+
+void all () {
+  double V { getSpeed () };
+  double D { displacement () };
+  double VpowerTwo { std :: pow ( V , 2 ) };
+  double accelaration { ( 0 - VpowerTwo ) /  ( 2 * D ) };
+  double M { getMass () };
+  std :: cout << "the stop accelaration of the car is : " << accelaration << " m / s2 " << '\n';
+
+  if ( V / accelaration > 0 ) 
+    std :: cout << "the time betweem breake and stop is : " << static_cast < int > ( V / accelaration )  << 's' << '\n';
+
+  else if ( V / accelaration < 0 ) 
+    std :: cout << "the time betweem breake and stop is : " << static_cast < int > ( V / accelaration ) * -1  << 's' << '\n';
+
+  if (  ( accelaration * M ) > 0 )
+    std :: cout << "the friction force between the tire and the surface is : " << accelaration * M<< 'N' <<'\n';
+
+  else if  (  ( accelaration * M ) < 0 )
+    std :: cout << "the friction force between the tire and the surface is : " <<  -1 * ( accelaration * M ) << 'N' <<'\n';
+
+  bool RN { makeRandomNumbers () };
+  
+  if ( RN ) {
+
+    int intA { static_cast < int > ( accelaration ) }; 
+    int intT { static_cast < int > ( ( V / accelaration > 0 ) ? V/ accelaration : -1 * ( V / accelaration ) ) };
+    int intFF { static_cast < int > ( ( accelaration * M > 0 ) ? accelaration * M : -1 * ( accelaration * M ) ) }; 
+    std :: cout << "1 : accelaration stop " << '\n';
+    std :: cout << "2 : time between break and stop " << '\n';
+    std :: cout << "3 : firction force between tire and surface " << '\n';
+    int I1 { static_cast < int > ( first_Item () ) };
+    int I2 { static_cast < int > ( second_Item () ) };
+    
+    if ( ( I1 == 1 && I2 == 2 ) || ( I1 == 2 && I2 == 1 ) ) {
+      std :: cout << "you entered item 1 and 2 " << '\n'; 
+      random ( intA , intT );
+
+    }
+
+    if ( ( I1 == 1 && I2 == 3 ) || ( I1 == 3 && I2 == 1 ) ) {
+      std :: cout << "you entered item 1 and 3 " << '\n'; 
+      random ( intA , intFF );
+    }
+
+    if ( ( I1 == 2 && I2 == 3 ) || ( I1 == 3 && I2 == 2 ) ) {
+      std :: cout << "you entered item 2 and 3 " << '\n'; 
+      random ( intT , intFF );
+    }
+
+
+
+  }
+
+  else
+    std :: cout << "you entered false " << '\n';
+
+
+}
+
+bool makeRandomNumbers () {
+  std :: cin >> std :: boolalpha;
+  std :: cout << "enter true : make random numbers between item 1 , 2 , 3 " << '\n';
+  std :: cout << "enter false : quit the progeram " << '\n'; 
+  bool tOrF {};
+  std :: cin >> tOrF;
+
+return tOrF;  
+}
+
+double first_Item() {
+  A:
+  std :: cout << "enter the first item : " << '\n';
+  double opt {};
+  std :: cin >> opt;
+
+  if ( opt != 1 && opt != 2 && opt != 3 ) {
+    std :: cout << "you must enter 1 , 2 , 3 " << '\n';
+    goto A;
+  }
+
+  if ( opt - static_cast < int > ( opt ) ) {
+    std :: cout << "do not enter floating point " << '\n';
+    goto A;
+
+  }
+
+return opt;
+}
+
+double second_Item() {
+  A:
+  std :: cout << "enter the second item : " << '\n';
+  double opt {};
+  std :: cin >> opt;
+
+  if ( opt != 1 && opt != 2 && opt != 3 ) {
+    std :: cout << "you must enter 1 , 2 , 3 " << '\n';
+    goto A;
+  }
+
+  if ( opt - static_cast < int > ( opt ) ) {
+    std :: cout << "do not enter floating point " << '\n';
+    goto A;
+
+  }
+
+return opt;
+}
+
+
+
+
 auto answer () {
   auto O { option () };
-  
+  double a;
+  double t;
+  double ff;
+
   switch ( static_cast < int > ( O ) ) {
 
     case 1 :
       std :: cout << "you entered 1 " << '\n';
-      break;
+      a = accelaration (); 
+      return;
 
     case 2 :
       std :: cout << "you entered 2 " << '\n';
-      break;
+      t = time (); 
+      return;
 
     case 3 :
       std :: cout << "you entered 3 " << '\n';
-      break;
+      ff = frictionForce (); 
+      return;
+
+    case 4 :
+      std :: cout << "you entered 4 " << '\n';  
+      all ();
+      return; 
 
   }
 
 }
-
-/*
-void evenFounder ( int num , int num2 ) {
-  std :: cout << "enter the number of the sequnce : " << '\n';
-  int SN {};
-  std :: cin >> SN;
-
-  std :: mt19937_64 random { static_cast < unsigned int > ( std :: chrono :: steady_clock :: now () . time_since_epoch () . count () ) };
-  std :: uniform_int_distribution period ( num , num2 );
-  std :: mt19937_64 random2 { random };
-  std :: mt19937_64 random3 { random };
-
-  for ( int counter { 1 }; counter <=SN; ++ counter ) {
-    std :: cout << period ( random ) << '\t';
-
-    if ( counter % 10 == 0 ) 
-      std :: cout << '\n';
-
-
-  }
-   
-  std :: cout << '\n';
-  std :: cout << '\n'; 
-  
-  if ( period ( random2 ) % 2 == 0 ) {
-    std :: cout << " evens : " << '\n';
-
-    for ( int counter { 1 }; counter <=SN; ++ counter ) {
-      std :: cout << period ( random2 ) << " = " << period ( random3 ) % 2 << '\t';
-
-      if ( counter % 10 == 0 ) 
-        std :: cout << '\n';
-
-
-    }
-  
-  }
-
-}
-*/
 
 
 int main () {
-
-  random ( 30 , 10 ); 
-
+  car ();
+  answer (); 
+  std :: cout << "the bulid end " << '\n';
+  std :: cout << "press any key to quit " << '\n';
+  std :: string key {};
+  std :: cin >> key;
 
 return 0; 
 }
-
-  /*
-  std :: mt19937_64 random { static_cast < unsigned int > ( std :: chrono :: steady_clock :: now () . time_since_epoch () . count () ) };
-  std :: uniform_int_distribution D ( 10 , 20 );
-  std :: mt19937_64 random2 { random };
-
-  for ( int counter { 1 }; counter <=20; ++ counter ) {
-    std :: cout << D ( random ) << '\t';
-
-    if ( counter % 10 == 0 )
-      std :: cout << '\n';
-
-  }
-
-  std :: cout << '\n';
-
-  for ( int counter { 1 }; counter <=20; ++ counter ) {
-    std :: cout << D ( random2 ) << '\t';
-
-    if ( counter % 10 == 0 )
-      std :: cout << '\n';
-
-  }
-*/
-
