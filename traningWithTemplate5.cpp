a6cbf7fddc9fa8b69680a5e2deeb5909659cfedc
lamar davis <155438522+lamarDavisgta5@users.noreply.github.com>
2024-01-10 04:40:06 -0800

    Update traningWithTemplate5.cpp
    the is updeted but it hasnt finished yet 
    compiled with gcc
    write on standerd C++17

--- traningWithTemplate5.cpp
+++ traningWithTemplate5.cpp
@@ -80,10 +80,38 @@ char option2 () {
   }
 }
 
+double getPower () {
+  enter :
+  std :: cout << "enter a number to power your random numbers : " << '\n';
+  double P {};
+  std :: cin >> P;
+
+  if ( P - static_cast < int > ( P ) != 0 ) {
+    std :: cout << "you must enter an intger number " << '\n';
+    goto enter;
+
+  } 
+
+return P;
+}
+
+bool trueOrFalse () {
+  std :: cin >> std :: boolalpha; 
+  std :: cout << "enter true : find the even and odd numbers " << '\n';
+  std :: cout << "enter flase : quit the progeram " << '\n';
+  bool tOrF {};
+  std :: cin >> tOrF;
+
+return tOrF;
+}
+
 template < typename T > 
-T random ( T object1 , T object2 ) {
+void random ( T object1 , T object2 ) {
   auto SN { getSequnceNumber () };
   std :: mt19937_64 sids { static_cast < unsigned int > ( std :: chrono :: steady_clock :: now () . time_since_epoch () . count () ) };
+  std :: mt19937_64 random2 { sids };
+  std :: mt19937_64 random3 { random2 };
+  std :: mt19937_64 random4 { random3 };
 
   if ( object1 > object2 ) {
     std :: cout << "the random number between " << object2 << " and " << object1 << " is : " << '\n';
@@ -99,6 +127,87 @@ T random ( T object1 , T object2 ) {
 
     std :: cout << '\n';
     
+    int O2 { option2 () };
+
+    if ( O2 == 80 || O2 == 112 ) {
+      std :: cout << "you entered to power you random numbers : " << '\n';
+      std :: cout << '\n';
+      int P { static_cast < int > ( getPower () ) };
+      std :: cout << "the power of the random numbers is : " << '\n';
+
+      for ( int counter { 1 }; counter <= SN; ++ counter ) {
+        std :: cout << std :: pow ( period ( random2 ) , P ) << '\t';
+
+        if ( counter % 10 == 0 )
+          std :: cout << '\n';
+
+
+      }
+
+      std :: cout << '\n';
+      bool tOrF { trueOrFalse () };
+
+      if ( tOrF ) {
+        std :: cout << "you entered true " << '\n';
+        std :: cout << '\n';
+        std :: cout << "odds : 1 " << '\n';
+        std :: cout << "evens : 0 " << '\n';
+
+        for ( int counter { 1 }; counter <= SN; ++ counter ) {
+          std :: cout << period ( random3 ) << " = " << period ( random4 ) % 2 << '\t';
+
+          if ( counter % 10 == 0 )
+            std :: cout << '\n';
+
+
+        }
+
+      }
+
+      else  
+        std :: cout << "you entered false " << '\n';  
+
+      
+
+
+    }
+
+    else if ( O2 == 82 || O2 == 114 ) {
+      std :: cout << "you entered to root the random numbers " << '\n';
+      std :: cout << '\n';
+      std :: cout << "the root of the random number is : " << '\n';
+
+      for ( int counter { 1 }; counter <= SN; ++ counter ) {
+        std :: cout << std :: sqrt ( period ( random2 ) ) << '\t';
+
+        if ( counter % 10 == 0 )
+          std :: cout << '\n';
+
+      }
+
+      std :: cout << '\n'; 
+      bool tOrF { trueOrFalse () };
+
+      if ( tOrF ) {
+        std :: cout << "you entered true " << '\n';
+        std :: cout << "odds : 1 " << '\n';
+        std :: cout << "evens : 0 " << '\n';
+        
+        for ( int counter { 1 }; counter <= SN; ++ counter ) {
+          std :: cout << period ( random3 ) << " = " << period ( random4 ) % 2 << '\t';
+
+          if ( counter % 10 == 0 ) 
+            std :: cout << '\n'; 
+
+        }
+
+      }
+
+      else 
+        std :: cout << "you entered false " << '\n';
+
+          
+    }
   
   }
 
@@ -119,21 +228,39 @@ T random ( T object1 , T object2 ) {
 
     if ( O2 == 80 || O2 == 112 ) {
       std :: cout << "you entered to power the random numbers " << '\n';
-      std :: mt19937_64 random2 { static_cast < unsigned int > ( period ( sids ) ) };
+      std :: cout << '\n'; 
+      auto power { getPower () }; 
       std :: cout << "the power of the random number between " << object1 << " and " << object2 << " is : " << '\n'; 
 
       for ( int counter { 1 }; counter <= SN; ++ counter ) {
-        std :: cout << random2 << '\t';
+        std :: cout <<  std :: pow ( ( period ( random2 ) ) , power ) << '\t';
 
         if ( counter % 10 == 0 ) 
           std :: cout << '\n'; 
 
       }
 
+      std :: cout << '\n'; 
+
+  
+
     }
 
     else if ( O2 == 82 || O2 == 114 ) {
       std :: cout << "you entered to root the random numbers " << '\n'; 
+      std :: cout << "the root of the rnadom numbers between " << object1 << " and " << object2 << " is : " << '\n';
+
+      for ( int counter { 1 }; counter <= SN; ++ counter ) {
+        std :: cout << std :: sqrt ( period ( random2 ) ) << '\t';
+
+        if ( counter % 10 == 0 )
+          std :: cout << '\n';
+
+
+      } 
+
+      std :: cout << '\n';
+
 
     }
 
@@ -166,9 +293,56 @@ auto answer () {
 
 }
 
+/*
+void evenFounder ( int num , int num2 ) {
+  std :: cout << "enter the number of the sequnce : " << '\n';
+  int SN {};
+  std :: cin >> SN;
+
+  std :: mt19937_64 random { static_cast < unsigned int > ( std :: chrono :: steady_clock :: now () . time_since_epoch () . count () ) };
+  std :: uniform_int_distribution period ( num , num2 );
+  std :: mt19937_64 random2 { random };
+  std :: mt19937_64 random3 { random };
+
+  for ( int counter { 1 }; counter <=SN; ++ counter ) {
+    std :: cout << period ( random ) << '\t';
+
+    if ( counter % 10 == 0 ) 
+      std :: cout << '\n';
+
+
+  }
+   
+  std :: cout << '\n';
+  std :: cout << '\n'; 
+  
+  if ( period ( random2 ) % 2 == 0 ) {
+    std :: cout << " evens : " << '\n';
+
+    for ( int counter { 1 }; counter <=SN; ++ counter ) {
+      std :: cout << period ( random2 ) << " = " << period ( random3 ) % 2 << '\t';
+
+      if ( counter % 10 == 0 ) 
+        std :: cout << '\n';
+
+
+    }
+  
+  }
+
+}
+*/
 
 
 int main () {
+
+  random ( 30 , 10 ); 
+
+
+return 0; 
+}
+
+  /*
   std :: mt19937_64 random { static_cast < unsigned int > ( std :: chrono :: steady_clock :: now () . time_since_epoch () . count () ) };
   std :: uniform_int_distribution D ( 10 , 20 );
   std :: mt19937_64 random2 { random };
@@ -190,7 +364,5 @@ int main () {
       std :: cout << '\n';
 
   }
+*/
 
-
-return 0; 
-}
